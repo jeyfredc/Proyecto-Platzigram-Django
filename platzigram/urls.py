7d4669508ef81067e2e49d802a46482c875dd6dd
Jeyfredc <jeyfredc@gmail.com>
2020-10-28 15:35:06 -0500

    Configuraciones

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -15,5 +15,4 @@ urlpatterns = [
 
     path('users/', include(('users.urls', 'users'), namespace='users')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
