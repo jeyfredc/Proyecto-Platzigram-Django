d53309d7504a7ed4a06ca887e344bcb9aaee4590
Jeyfredc <jeyfredc@gmail.com>
2020-10-28 15:41:59 -0500

    Configuraciones

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = (BASE_DIR/ 'staticfiles')
+STATIC_ROOT = (BASE_DIR/, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR/ 'static']
+STATICFILES_DIRS = [BASE_DIR/, 'static']
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
